c7e1e212a1b7d4cc0945e46668f27db8066e9ef2
Taufik Hidayat <hidytt.taufik@gmail.com>
2024-02-17 00:51:22 +0700

    update

--- insertBarang.sql
+++ insertBarang.sql
@@ -1,2 +1,14 @@
 INSERT INTO barang ( id_kategori, nama_barang, description_barang, harga_barang, created_at ) 
-VALUES (1, 'jersey timnas indonesia', 'ini description jersey timnas indonesia', 120000, '2024-02-14 18:00:00');
+VALUES 
+(1, 'jersey timnas indonesia', 'ini description jersey timnas indonesia', 120000, '2024-02-14 18:00:00'),
+(1, 'jersey timnas beazil', 'ini description jersey timnas brazil', 120000, '2024-02-14 18:00:00'),
+(1, 'jersey timnas japan', 'ini description jersey timnas japan', 120000, '2024-02-14 18:00:00'),
+(1, 'jersey timnas jerman', 'ini description jersey timnas jerman', 120000, '2024-02-14 18:00:00'),
+(2, 'palu thor', 'ini description palu thor', 100000, '2024-02-14 18:00:00'),
+(2, 'gergaji besi', 'ini description gergaji besi', 120000, '2024-02-14 18:00:00'),
+(2, 'tang jepit', 'ini description tang jepit', 120000, '2024-02-14 18:00:00'),
+(3, 'sepatu gunung', 'ini description sepatu gunung', 120000, '2024-02-14 18:00:00'),
+(3, 'sepatu sekolah', 'ini description sepatu sekolah', 120000, '2024-02-14 18:00:00'),
+(3, 'sepatu running', 'ini description sepatu running', 120000, '2024-02-14 18:00:00'),
+(4, 'smart tv', 'smart tv', 120000, '2024-02-14 18:00:00'),
+(4, 'mesin cuci', 'ini description mesin cuci', 120000, '2024-02-14 18:00:00');
--- insertKategori.sql
+++ insertKategori.sql
@@ -1,2 +1,6 @@
 INSERT INTO kategori ( nama_kategori, description_kategori, created_at )
-VALUES ('pakaian', 'ini description pakaian', '2024-02-14 18:00:00');
+VALUES 
+('pakaian', 'ini description pakaian', '2024-02-14 18:00:00'),
+('perkakas', 'ini description perkakas', '2024-02-14 18:00:00'),
+('sepatu', 'ini description sepatu', '2024-02-14 18:00:00'),
+('elektronik', 'ini description elektronik', '2024-02-14 18:00:00');
